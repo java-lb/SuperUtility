e28ab891eb6eb19074a6c48639b76b8ac5be194e
lblovepm <18557539048@163.com>
2019-05-13 15:44:09 +0800

    修改一些东西

--- src/org/xdemo/superutil/thirdparty/XmlUtils.java
+++ src/org/xdemo/superutil/thirdparty/XmlUtils.java
@@ -13,7 +13,7 @@ import org.xdemo.superutil.test.User;
  * XML序列化与反序列化工具类
  * @author Mr.LB
  * 2015年2月28日
- * @see   <a href="http://simple.sourceforge.net/download/stream/doc/tutorial/tutorial.php">Simple-XML</a>
+ * @see <a href="http://simple.sourceforge.net/download/stream/doc/tutorial/tutorial.php">Simple-XML</a>
  */
 public class XmlUtils {
 	
